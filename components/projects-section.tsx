dbb5e4f521c0cc80ff5a777e0539993d243aaaaa
AhadAhmed56 <ahadahmed453266@gmail.com>
2026-01-26 00:20:39 +0500

    introduce HeroParallax component for project display and update Fancybox dependency.

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -12,21 +12,18 @@ export const products = [
     link: "https://gomoonbeam.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
-    category: "Web Design",
   },
   {
     title: "Cursor",
     link: "https://cursor.so",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/cursor.png",
-    category: "SaaS",
   },
   {
     title: "Rogue",
     link: "https://userogue.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-    category: "Logo Design",
   },
 
   {
@@ -34,21 +31,18 @@ export const products = [
     link: "https://editorially.org",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/editorially.png",
-    category: "Web Design",
   },
   {
     title: "Editrix AI",
     link: "https://editrix.ai",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/editrix.png",
-    category: "SaaS",
   },
   {
     title: "Pixel Perfect",
     link: "https://app.pixelperfect.quest",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-    category: "Logo Design",
   },
 
   {
@@ -56,35 +50,30 @@ export const products = [
     link: "https://algochurn.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-    category: "Web Design",
   },
   {
     title: "Aceternity UI",
     link: "https://ui.aceternity.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-    category: "SaaS",
   },
   {
     title: "Tailwind Master Kit",
     link: "https://tailwindmasterkit.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-    category: "Web Design",
   },
   {
     title: "SmartBridge",
     link: "https://smartbridgetech.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-    category: "SaaS",
   },
   {
     title: "Renderwork Studio",
     link: "https://renderwork.studio",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-    category: "Logo Design",
   },
 
   {
@@ -92,28 +81,23 @@ export const products = [
     link: "https://cremedigital.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-    category: "Web Design",
   },
   {
     title: "Golden Bells Academy",
     link: "https://goldenbellsacademy.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-    category: "SaaS",
   },
   {
     title: "Invoker Labs",
     link: "https://invoker.lol",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-    category: "Logo Design",
   },
   {
     title: "E Free Invoice",
     link: "https://efreeinvoice.com",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-    category: "SaaS",
   },
 ];
-
